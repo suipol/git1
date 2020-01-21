963342ccdfe1ad28dc6fe2d4ff941d45cb90b70a
linhua <1756471771@qq.com>
2020-01-21 17:10:12 +0800

    first commit

---
+++ firstvue/vue_test/src/components/hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1 v-if="first">{{ msg }}</h1>
+    <input type="checkbox" v-model="first" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '欢迎',
+      first: false
+    }
+  }
+}
+</script>
